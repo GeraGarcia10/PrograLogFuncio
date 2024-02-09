b190f712481ebe2deb5da2fc22fbb7ce6b0f6472
GeraGarcia10 <159507563+GeraGarcia10@users.noreply.github.com>
2024-02-08 23:36:09 -0600

    Archivo 1 FizzBuzz
    Se subio el archivo de clase del trabajo "Kata: FizzBuzz"

---
+++ FizzBuzz.hs
@@ -0,0 +1,31 @@
+module FizzBuzz where
+
+fizzBuzz :: Int -> String
+fizzBuzz n
+  | n `mod` 3 == 0 && n `mod` 5 == 0 = "fizzbuzz!"
+  | n `mod` 3 == 0 = "fizz!"
+  | n `mod` 5 == 0 = "buzz!"
+  | otherwise = number n ++ "!"
+
+lessThan :: Int -> String
+lessThan n
+  | n > 0 && n < 20 =
+    let lessThanAnswers =
+          words ("one two three four five six seven eight nine ten " ++
+                 "eleven twelve thirteen fourteen fifteen sixteen "  ++
+                 "seventeen eighteen nineteen")
+    in lessThanAnswers !! (n-1)
+
+tens :: Int -> String
+tens n
+  | n >= 2 && n <= 9 =
+    tensAnswers !! (n-2)
+    where
+      tensAnswers = words "twenty thirty forty fifty sixty seventy eighty ninety"
+
+number :: Int -> String
+number n
+  | 1 <= n && n < 20 = lessThan n
+  | n `mod` 10 == 0 && n < 100 = tens (n `div` 10)
+  | n < 100 = tens (n `div` 10) ++ " " ++ lessThan (n `mod` 10)
+  | n == 100 = "one hundred"
